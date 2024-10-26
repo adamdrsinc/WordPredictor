916443edc9f59aa33987f9708c9be86c4333f6d9
adamdrsinc <adam.sinc124@gmail.com>
2024-10-26 16:13:28 -0600

    Add project files.

---
+++ WordTree.cpp
@@ -0,0 +1,76 @@
+#include "WordTree.hpp"
+
+void WordTree::add(std::string word) {
+	//If word is empty, ignore it
+	if (word.size() <= 0)
+	{
+		return;
+	}
+
+	for (int i = 0; i < word.length(); i++)
+	{
+		char c = word[i];
+
+		//If word contains non-alphabetic characters, ignore it
+		if (!std::isalpha(c))
+		{
+			return;
+		}
+
+		word[i] = tolower(c);
+	}
+
+
+		/*
+	Get array of nodes
+	Check if node corresponding to letter exists
+		If it does, traverse to that node
+		If it doesn't, create a new node and traverse to that node
+	At the end, set boolean to true
+	*/
+
+	int idx = word[0] - 97;
+	std::shared_ptr<TreeNode> currentNode = tree[idx];
+
+	if (tree[idx] == nullptr)
+	{
+		tree[idx] = std::make_shared<TreeNode>();
+		currentNode = tree[idx];
+	}
+
+
+	for (int i = 1; i <= word.size(); i++)
+	{
+		if (i == word.size())
+		{
+			currentNode->children[idx] = std::make_shared<TreeNode>();
+			currentNode->endOfWord = true;
+			break;
+		}
+
+		int idx = word[i] - 97;
+
+		if (currentNode->children[idx] != nullptr)
+		{
+			currentNode = currentNode->children[idx];
+		}
+		else
+		{
+			currentNode->children[idx] = std::make_shared<TreeNode>();
+			currentNode = currentNode->children[idx];
+		}
+	}
+}
+
+//bool WordTree::find(std::string word) {
+//}
+//
+////Given the partial word, returns up to howMany predicted words
+//std::vector<std::string> WordTree::predict(std::string partial, std::uint8_t howMany) {
+//
+//}
+//	
+////Returns the number of words in the tree
+//std::size_t WordTree::size() {
+//
+//}
---
+++ WordTree.hpp
@@ -0,0 +1,54 @@
+#pragma once
+
+#include <string>
+#include <vector>
+#include <array>
+#include <memory>
+
+class TreeNode
+{
+private:
+public:
+	bool endOfWord = false;
+	std::array<std::shared_ptr<TreeNode>, 26> children;
+};
+
+class WordTree
+{
+private:
+	std::array<std::shared_ptr<TreeNode>, 26> tree;
+
+public:
+	void add(std::string word);
+	//bool find(std::string word);
+
+	//Given the partial word, returns up to howMany predicted words
+	//std::vector<std::string> predict(std::string partial, std::uint8_t howMany);
+
+	//Returns the number of words in the tree
+	//std::size_t size();
+
+};
+
+/*
+
+std::string word = "abc";
+
+std::array<std::shared_ptr<TreeNode>, 26> tree;
+
+[0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10... 25]
+
+firstLetter = word[0]; //g
+index = firstLetter - 97;
+
+wordWithoutLetter = word.substr(1, word.length() - 1); //bc
+
+tree[index] = std::make_shared<TreeNode>(wordWithoutLetter);
+tree[0] is now filled. This represents 'a'.
+
+This should 
+
+
+*/
+
+
